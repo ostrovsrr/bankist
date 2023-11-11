9d97f737bd25b2899cb4aebb1fa46637ba481fa9
Rodion Ostrovskii <ostrovsrr@gmail.com>
2023-11-10 22:47:24 -0500

    update user credentials

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
-/////////////////////////////////////////////////
-// Data
-
-// DIFFERENT DATA! Contains movement dates, currency and locale
-
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Rodion Ostrovskii',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -30,7 +21,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'John Annichini',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
